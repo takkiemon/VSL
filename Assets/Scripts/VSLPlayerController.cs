c7425739423333efc75763c76f2d931a473c4fdf
takmanfa <takman@facilityapps.com>
2025-07-13 15:47:33 +0200

    change VSLPlayerController name and remove redundant PlayerController

--- Assets/Scripts/VSLPlayerController.cs
+++ Assets/Scripts/VSLPlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class VSLPlayerController : MonoBehaviour {
 
 	private Rigidbody rigidBody;
 
